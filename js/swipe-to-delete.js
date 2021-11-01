c1bcbe377c3feec2445b39af961c20177658af40
CecileAnna <6263325@rts-365.dk>
2021-11-01 11:28:15 +0100

    ja work commit (coordinates)

--- js/swipe-to-delete.js
+++ js/swipe-to-delete.js
@@ -1,5 +1,22 @@
 const swipeSentence = document.querySelector('#swipe-sentence');
 
-swipeSentence.addEventListener('touchStart', () => {
-    console.log('touched');
+let touchStartCoordinateX;
+let touchMoveCoordinateX;
+
+swipeSentence.addEventListener('touchstart', (event) => {
+
+    touchStartCoordinateX = event.touches[0].clientX;
+
+    // console.log(touchStartCoordinateX);
+
+
+
+});
+
+swipeSentence.addEventListener('touchmove', (event) => {
+
+    touchMoveCoordinateX = event.touches[0].clientX;
+
+    // console.log(touchMoveCoordinateX);
+
 });
