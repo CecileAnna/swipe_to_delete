3004f29e1603c19fec8030e797f81f945b4ba3eb
CecileAnna <6263325@rts-365.dk>
2021-11-01 15:06:52 +0100

    js changes commit

--- js/swipe-to-delete.js
+++ js/swipe-to-delete.js
@@ -2,7 +2,11 @@ const swipeSentence = document.querySelector('#swipe-sentence');
 
 let touchStartCoordinateX;
 
+let touchEndCoordinateX;
+
 let touchMoveCoordinateX;
+
+let deleteButtonWidth;
 
 swipeSentence.addEventListener('touchstart', (event) => {
 
@@ -18,13 +22,33 @@ swipeSentence.addEventListener('touchmove', (event) => {
 
     // console.log(touchMoveCoordinateX);
 
-    let deleteButtonWidth = (window.screen.width * 40) / 100;
+    deleteButtonWidth = (window.screen.width * 30) / 100;
 
     if (touchMoveCoordinateX < touchStartCoordinateX && touchMoveCoordinateX > touchStartCoordinateX - deleteButtonWidth) {
 
         swipeSentence.style.transform = `translateX(${touchMoveCoordinateX - touchStartCoordinateX}px)`;
         swipeSentence.style.transition = `0.1s ease-in-out`;
 
+    }
+
+});
+
+swipeSentence.addEventListener('touchend', (event) => {
+
+    touchEndCoordinateX = Math.floor(event.changedTouches[0].clientX);
+
+    // console.log(touchEndCoordinateX);
+    // console.log(touchStartCoordinateX);
+    // console.log(deleteButtonWidth);
+
+    if (touchEndCoordinateX < touchStartCoordinateX - deleteButtonWidth / 2) {
+
+        // console.log('teeest');
+
+        swipeSentence.style.transform = `translateX(-${deleteButtonWidth}px)`;
+
+    } else {
+        swipeSentence.style.transform = `translateX(${0})`;
     }
 
 });
